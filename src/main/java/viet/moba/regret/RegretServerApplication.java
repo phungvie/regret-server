1a024f47b886e098413efc63c88be28868e3e33f
phungvie <phungtienviet14@gmail.com>
2025-05-10 19:14:17 +0700

    first commit

---
+++ src/main/java/viet/moba/regret/RegretServerApplication.java
@@ -0,0 +1,13 @@
+package viet.moba.regret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegretServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegretServerApplication.class, args);
+	}
+
+}
